adea77529b148c3f5d3c6bbc6a24b575afcdc397
Amanda Bull <ms.amandabull@outlook.com>
2021-08-23 13:46:35 -0500

    Math2

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -2,3 +2,8 @@ function addition_Function() { //Defining a function and naming it
     var addition = 2 + 2; //Defining a variable and giving it an expression to simplify
     document.getElementById("Math").innerHTML = "2 + 2 = " + addition; //Creating a string to reflect the results of the element ID "Math"
 }
+
+function subtraction_Function() { //Defining a function and naming it
+    var subtraction = 5 - 2; //Defining a variable and giving it an expression to simplify
+    document.getElementById("Math2").innerHTML = "5 - 2 = " + subtraction; //Creating a string to reflect the results of the element ID "Math2"
+}
