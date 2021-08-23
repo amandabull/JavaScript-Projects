686d5b28a7846318870ea553a9eccbd2f0c9fe15
Amanda Bull <ms.amandabull@outlook.com>
2021-08-23 13:22:36 -0500

    Added comments

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,11 +1,11 @@
 function My_First_Function() { //Defining a function and naming it
     var str = "This text is green!"; //Defining a variable and giving it a string value
-    var result = str.fontcolor("green"); //Using the fontcolo methon on str variable
+    var result = str.fontcolor("green"); //Using the fontcolor method on str variable
     document.getElementById("Green_Text").innerHTML = result; //Putting the value of result into HTML element with "Green_Text" id
 }
 
-function myFunction() {
-    var sentence = "I am learning";
-    sentence+= " a lot from this book!";
-    document.getElementById("Concatenate").innerHTML = sentence;
+function myFunction() { //Defining a function and naming it
+    var sentence = "I am learning"; //Defining a variable and giving it a string value
+    sentence+= " a lot from this book!"; //Concatening another string to the variable sentence
+    document.getElementById("Concatenate").innerHTML = sentence; //Putting the value of result into HTML element with "Concatenate" id
 }
