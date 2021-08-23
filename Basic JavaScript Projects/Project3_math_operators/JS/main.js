7308bdbc5568a414fbbe583ea2686aac0e06cf9b
Amanda Bull <ms.amandabull@outlook.com>
2021-08-23 13:43:31 -0500

    Math

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,4 @@
+function addition_Function() { //Defining a function and naming it
+    var addition = 2 + 2; //Defining a variable and giving it an expression to simplify
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition; //Creating a string to reflect the results of the element ID "Math"
+}
